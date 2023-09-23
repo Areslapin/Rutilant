f50d11f42e771e8e4f29ea8c40d297f5f4ff4e50
Areslapin <jpgerbault@gmail.com>
2023-09-24 00:28:54 +0200

    JP2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Card from './components/Card'
-import './App.css'
+// Define arrays for "Back" and "Value" values
+const backValues = [1, 2, 3];
+const valueValues = [1, 2, 3];
 
-function App() {
-
-  return (
-    <>
-    <Card value={
-      1
-    } 
-    />
-    </>
-  )
+// Shuffle the arrays (Fisher-Yates shuffle)
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
 
-export default App
+shuffleArray(backValues);
+shuffleArray(valueValues);
+
+// Create and assign values to three cards
+const cards = [];
+
+for (let i = 0; i < 3; i++) {
+  const card = {
+    back: backValues.pop(),
+    value: valueValues.pop(),
+  };
+  cards.push(card);
+}
+
+// Print the card values
+cards.forEach((card, index) => {
+  console.log(`Card ${index + 1}: Back - ${card.back}, Value - ${card.value}`);
+});
+
+export default App; // Default export
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // Import the default export without curly braces
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
