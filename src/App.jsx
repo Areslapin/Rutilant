dbb4e4a7ba6a0188223dfb7999b48ee68587e2c1
Maxime Kerlidou <87577502+MaxFmr@users.noreply.github.com>
2023-09-24 17:59:41 +0200

    le jeu marche et JP n'aime pas les prouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,83 @@
 import Card from './components/Card';
+import { useState, useEffect } from 'react';
+import './App.css';
 
 const App = () => {
-  const backValues = [1, 2, 3];
-  const valueValues = [1, 2, 3];
+  const [cardsClicked, setCardsClicked] = useState({
+    card1: false,
+    card2: false,
+    card3: false,
+  });
 
-  // Shuffle the arrays (Fisher-Yates shuffle)
+  const [cards, setCards] = useState([]);
+
+  const [isLost, setIsLost] = useState(false);
+  const [firstStep, setFirstStep] = useState(true);
+  const [secondStep, setSecondStep] = useState(false);
+  const [thirdStep, setThirdStep] = useState(false);
+
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
   }
+  useEffect(() => {
+    if (isLost) {
+      setCardsClicked({
+        card1: false,
+        card2: false,
+        card3: false,
+      });
+      alert('perdu');
+    }
+  }, [isLost]);
 
-  shuffleArray(backValues);
-  shuffleArray(valueValues);
+  useEffect(() => {
+    const backValues = [1, 2, 3];
+    const valueValues = [1, 2, 3];
 
-  // Create and assign values to three cards
-  const cards = [];
+    shuffleArray(backValues);
+    shuffleArray(valueValues);
 
-  for (let i = 0; i < 3; i++) {
-    const card = {
-      back: backValues.pop(),
-      value: valueValues.pop(),
-    };
-    cards.push(card);
-  }
+    const array = [];
 
-  // Print the card values
-  cards.forEach((card, index) => {
-    console.log(
-      `Card ${index + 1}: Back - ${card.back}, Value - ${card.value}`
-    );
-  });
+    for (let i = 0; i < 3; i++) {
+      const card = {
+        back: backValues.pop(),
+        value: valueValues.pop(),
+      };
+      array.push(card);
+    }
+    setCards(array);
+  }, []);
+
+  useEffect(() => {
+    if (cardsClicked.card1 && cardsClicked.card2 && cardsClicked.card3) {
+      console.log('You win!');
+    }
+  }, [cardsClicked]);
 
   return (
     <>
       {cards &&
         cards.map((card, index) => {
-          return <Card key={index} card={card} />;
+          return (
+            <Card
+              key={index}
+              card={card}
+              cardsClicked={cardsClicked}
+              setCardsClicked={setCardsClicked}
+              isLost={isLost}
+              setIsLost={setIsLost}
+              firstStep={firstStep}
+              setFirstStep={setFirstStep}
+              secondStep={secondStep}
+              setSecondStep={setSecondStep}
+              thirdStep={thirdStep}
+              setThirdStep={setThirdStep}
+            />
+          );
         })}
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,87 @@
-const Card = ({ card }) => {
+import { useState } from 'react';
+
+const Card = ({
+  card,
+  cardsClicked,
+  setCardsClicked,
+  isLost,
+  setIsLost,
+  firstStep,
+  setFirstStep,
+  secondStep,
+  setSecondStep,
+  thirdStep,
+  setThirdStep,
+}) => {
+  const [isClickable, setIsClickable] = useState(true);
+  const [fliped, setFliped] = useState(false);
+
   const BackPath = `src/assets/Carrds/back/0${card.back}_Back.png`;
+  const FrontPath = `src/assets/Carrds/front/0${card.value}_Front.png`;
+
+  const playCard = () => {
+    if (firstStep && isClickable) {
+      firstStepPlay();
+    }
+
+    if (secondStep && isClickable) {
+      secondStepPlay();
+    }
+    if (thirdStep && isClickable) {
+      thirdStepPlay();
+    }
+  };
+
+  const firstStepPlay = () => {
+    if (card.value === 1 && firstStep) {
+      setFirstStep(false);
+      setSecondStep(true);
+      setCardsClicked({ ...cardsClicked, card1: true });
+      setIsClickable(false);
+      alert('1');
+      setFliped(true);
+    } else {
+      console.log('perdu');
+      setIsLost(true);
+    }
+  };
+  const secondStepPlay = () => {
+    if (card.value === 2 && cardsClicked.card1) {
+      setSecondStep(false);
+      setThirdStep(true);
+      setCardsClicked({ ...cardsClicked, card2: true });
+      setIsClickable(false);
+      alert('2');
+      setFliped(true);
+    } else {
+      console.log('perdu2');
+      setIsLost(true);
+    }
+  };
+  const thirdStepPlay = () => {
+    if (card.value === 3 && cardsClicked.card2 && cardsClicked.card1) {
+      setCardsClicked({ ...cardsClicked, card3: true });
+      setIsClickable(false);
+      alert('you win');
+      setCardsClicked({
+        card1: false,
+        card2: false,
+        card3: false,
+      });
+      setThirdStep(false);
+      setFirstStep(true);
+      setFliped(true);
+    }
+  };
 
   return (
     <>
-      <div className='card'>
-        <img src={BackPath} alt='Carte' />
-        <span className='card-value'>{card.value}</span>
+      <div className='card' onClick={() => playCard()}>
+        {!fliped ? (
+          <img src={BackPath} alt='Carte' />
+        ) : (
+          <img src={FrontPath} alt='Carte' />
+        )}{' '}
       </div>
     </>
   );
